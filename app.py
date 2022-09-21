b15761d03aa9b957bf8e906149fd6d08743e24f9
shinjw <cobalt41g@gmail.com>
2022-09-21 17:06:48 +0900

    add create_project

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from functools import wraps
+from unittest import result
 from flask import Flask, jsonify, request, Response, g
 from pymongo import MongoClient
 from bson.objectid import ObjectId
@@ -21,6 +22,7 @@ def get_user(user_id):
             "name": user["name"],
             "password": user["password"],
             "description": user["description"],
+            "progress": user["progress"],
         }
         if user
         else None
@@ -52,7 +54,7 @@ def login_required(f):
     return decorated_function
 
 
-def get_user_by_email(email):
+def find_user_by_email(email):
     return db.users.find_one({"email": email})
 
 
@@ -60,6 +62,23 @@ def get_user_by_email(email):
 @login_required
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/users", methods=["GET"])
+def get_user_by_email():
+    args = request.args
+    email = args.get("email")
+    user = find_user_by_email(email)
+
+    if user is None:
+        return "", 404
+
+    payload = {
+        "email": user["email"],
+        "name": user["name"],
+    }
+
+    return jsonify(payload)
 
 
 @app.route("/sign-up", methods=["POST"])
@@ -76,7 +95,9 @@ def sign_up():
         if form[x] == "":
             raise Exception("회원가입 정보가 충분하지 않습니다.")
 
-    if get_user_by_email(new_user["email"]):
+    new_user["progress"] = []
+
+    if find_user_by_email(new_user["email"]):
         return "이미 사용중인 email 입니다."
 
     db.users.insert_one(new_user)
@@ -84,25 +105,79 @@ def sign_up():
     return render_template("login.html")
 
 
-@app.route("/sign-up-page")
+@app.route("/sign-up", methods=["GET"])
 def render_sign_up_page():
     return render_template("signup.html")
 
 
-@app.route("/login-page")
+@app.route("/login", methods=["GET"])
 def render_login_page():
     return render_template("login.html")
 
 
+@app.route("/projects", methods=["GET"])
+def render_prj_dashboard():
+    projects = [
+        {"aa": 1, "bb": 2},
+        {"aa": 11, "bb": 12},
+    ]
+    return render_template("prj_dashboard.html", projects=projects)
+
+
+@app.route("/projects", methods=["POST"])
+def create_project():
+    project = request.json
+
+    if project["name"] == "":
+        return "프로젝트 제목이 없습니다.", 400
+
+    if project["members"].__len__() == 0:
+        return "프로젝트 팀원이 존재하지 않습니다.", 400
+
+    members = project["members"]
+    members_without_duplicate = set(map(lambda m: m["email"], members))
+
+    if members.__len__() != members_without_duplicate.__len__():
+        return "", 400
+
+    for member in members:
+        user = find_user_by_email(member["email"])
+        if user is None:
+            return "", 400
+
+    result = db.projects.insert_one(project)
+    project_id = result.inserted_id
+
+    for member in members:
+        user = find_user_by_email(member["email"])
+        user_id = str(user["_id"])
+        progress_data = {
+            "projectId": project_id,
+            "solvedPosts": [],
+            "solved": False,
+        }
+        db.users.update_one(
+            {"_id": ObjectId(user_id)},
+            {"$push": {"progress": progress_data}},
+        )
+
+    return jsonify({"result": "success"})
+
+
+@app.route("/create-project", methods=["GET"])
+def render_create_prj_page():
+    return render_template("create_project.html")
+
+
 @app.route("/login", methods=["POST"])
 def login():
-    credential = request.json
+    credential = request.form
     email = credential["email"]
     password = credential["password"]
 
-    row = get_user_by_email(email)
+    row = find_user_by_email(email)
     if row == None or password != row["password"]:  # 단축 평가
-        return "", 401
+        return "", 401  # Unauthorized
 
     user_id = str(row["_id"])
     payload = {
@@ -112,12 +187,6 @@ def login():
 
     token = jwt.encode(payload, "MY_SECRET_KEY", "HS256")
     return jsonify({"access_token": token})
-
-
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello(name=None):
-    return render_template("hello.html", name=name)
 
 
 if __name__ == "__main__":
