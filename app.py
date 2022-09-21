00bdda4bed3105b434848cec3fda0a9afab92418
wnhlee <2wheeh@gmail.com>
2022-09-21 22:31:32 +0900

    connet post-dashboard and post-form

--- app.py
+++ app.py
@@ -291,6 +291,10 @@ def render_post_dashboard():
 
     return render_template("post-dashboard.html", post_metas = post_metas)
 
+@app.route("/post-form", methods=["GET"])
+def render_post_form():
+    return render_template("post-form.html")
+
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000, debug=True)
